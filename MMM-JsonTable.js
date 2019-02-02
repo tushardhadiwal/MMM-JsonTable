a9e8abc70c5d4ff56ad0bc3f4edbc0471044911c
Tushar D <tudhadiw@microsoft.com>
2019-02-01 16:37:58 -0800

    Added Json Parse and logic to parse voice commands & display section img

--- MMM-JsonTable.js
+++ MMM-JsonTable.js
@@ -13,6 +13,7 @@ Module.register("MMM-JsonTable", {
 	},
 
 	start: function () {
+		this.cam=0
 		this.getJson();
 		this.scheduleUpdate();
 	},
@@ -69,7 +70,52 @@ Module.register("MMM-JsonTable", {
 			return wrapper;
 		}
 
-		items.forEach(element => {
+
+		function pick(o, ...fields) {
+			return fields.reduce((a, x) => {
+				if(o.hasOwnProperty(x)) a[x] = o[x];
+				return a;
+			}, {});
+		}
+
+
+		var i,j;
+		var pitems=[];
+		var qitems=[];
+      for (i = 0,j=0; i < items.length; i++) { 
+
+		 if(items[i].deviceTelemetry !== null)
+		 {
+			pitems[j]=pick(items[i].deviceTelemetry.event,'device_id','image','crowd');
+			qitems[j]=pick(items[i].deviceTelemetry.event,'device_id','crowd');
+			j++;
+		 }
+
+	   }
+
+	//    var res = Math.max.apply(Math,items.map(function(o){return o.crowd;}));
+	//    var obj = items.find(function(o){ return o.crowd == res; })
+
+	//    items= sortBy(items, 'crowd');
+
+	// items= items.filter(x => x !== undefined);
+	// items= items.filter(function(e){return e}); 
+	   
+	function compareIndexFound(a, b) {
+		if (a.crowd > b.crowd) { return -1; }
+		if (a.crowd < b.crowd) { return 1; }
+		return 0;
+	  }
+	  pitems= pitems.sort(compareIndexFound);
+	  qitems= qitems.sort(compareIndexFound);
+
+	//   var pindex = pitems.findIndex(x => x.device_id==this.cam);
+	  
+	  this.cam= this.cam % pitems.length;
+	  this.sendNotification('SHOW_NEWIMAGE', {pay: pitems[this.cam].image});
+		
+
+		qitems.forEach(element => {
 			var row = this.getTableRow(element);
 			tbody.appendChild(row);
 		});
@@ -114,6 +160,141 @@ Module.register("MMM-JsonTable", {
 		else {
 			return input;
 		}
-	}
+	},
+
+
+	notificationReceived: function(notification, payload, sender) {
+
+        if (notification === 'SHOW_NEXT')
+        {
+			
+			Log.log('Notification Received from ' + sender.name + ' containing payload : ' );
+			this.cam=this.cam+1;
+			this.updateDom();
+		}
+		if (notification === 'SHOW_CAM')
+        {
+			var Small = {
+				'zero': 0,
+				'one': 1,
+				'two': 2,
+				'three': 3,
+				'four': 4,
+				'five': 5,
+				'six': 6,
+				'seven': 7,
+				'eight': 8,
+				'nine': 9,
+				'ten': 10,
+				'eleven': 11,
+				'twelve': 12,
+				'thirteen': 13,
+				'fourteen': 14,
+				'fifteen': 15,
+				'sixteen': 16,
+				'seventeen': 17,
+				'eighteen': 18,
+				'nineteen': 19,
+				'twenty': 20,
+				'thirty': 30,
+				'forty': 40,
+				'fifty': 50,
+				'sixty': 60,
+				'seventy': 70,
+				'eighty': 80,
+				'ninety': 90
+			};
+			
+			var Magnitude = {
+				'thousand':     1000,
+				'million':      1000000,
+				'billion':      1000000000,
+				'trillion':     1000000000000,
+				'quadrillion':  1000000000000000,
+				'quintillion':  1000000000000000000,
+				'sextillion':   1000000000000000000000,
+				'septillion':   1000000000000000000000000,
+				'octillion':    1000000000000000000000000000,
+				'nonillion':    1000000000000000000000000000000,
+				'decillion':    1000000000000000000000000000000000,
+			};
+			
+			var a, n, g;
+			function text2num(s) {
+				a = s.toString().split(/[\s-]+/);
+				n = 0;
+				g = 0;
+				a.forEach(feach);
+				return n + g;
+			}
+			
+			function feach(w) {
+				var x = Small[w];
+				if (x != null) {
+					g = g + x;
+				}
+				else if (w == "hundred") {
+					g = g * 100;
+				}
+				else {
+					x = Magnitude[w];
+					if (x != null) {
+						n = n + g * x
+						g = 0;
+					}
+					else { 
+						// alert("Unknown number: "+w); 
+						Log.log("Unknown number: "+w)
+					}
+				}
+			}
+
+			function isNumber(n) {
+				return !isNaN(parseFloat(n)) && isFinite(n);
+			  }
+
+			Log.log('Notification Received from ' + sender.name + ' containing payload : ' );
+			if (isNumber(payload.pay))
+			{
+				this.cam=payload.pay;
+			}
+			else
+			{
+				this.cam=text2num(payload.pay);
+			}
+
+			
+			this.updateDom();
+		}
+		
+
+		if (notification === 'SHOW_SECTION')
+        {
+			Log.log('Notification Received from ' + sender.name + ' containing payload : ' + payload.pay);
+			//ToDo: Update the parsing logic according the new API and update the following URL.
+			// this.config.url="https://thd-prod-backend.westus2.cloudapp.azure.com/api/sections/"+payload.pay+"/stats";
+			this.config.url="https://thd-prod-backend.westus2.cloudapp.azure.com/api/Devices/";
+			// this.config.url="https://thd-dev-andrey-backend.westus2.cloudapp.azure.com/api/v1/cameras?floor=F3"
+			this.getJson();
+			this.updateDom();
+			this.sendNotification('HIDE_HEATMAP');
+			this.sendNotification('SHOW_CROWD_COUNT');
+			this.show(100);
+
+		}
+
+		if (notification === 'HIDE_CROWD_COUNT') {
+			this.hide();
+		}  else if (notification === 'SHOW_CROWD_COUNT') {
+			this.show(100);
+		}
+		if ( notification === 'DOM_OBJECTS_CREATED') {
+			//now the Dom is ready; you can call hide() or show().
+			//  this.hide(0);
+		} 
+		
+
+        
+    }
 
 });
